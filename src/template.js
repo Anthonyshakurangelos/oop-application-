90dc9d0eaba4c2f881641342301b917ba143f253
Anthonyshakurangelos <anthonyshakurangelos@gmail.com>
2023-01-12 17:18:20 -0700

    added demo image and used bulma instead od bootsrap

--- src/template.js
+++ src/template.js
@@ -4,40 +4,69 @@ const html = [];
 
     const createManager = (manager) => {
     return `
-    <div class="card-body" style="width: 18rem;">
-         <h5 class="card-title">Manager: ${manager.getName()}</h5>
-         <h6 class="card-subtitle mb-2 "></h6>
-         <p class="card-text">Employee ID ${manager.getId()}</p>
-         <a href="#" class="card-link">Email: ${manager.getEmail()}</a>
-         <a href="#" class="card-link">Office Number: ${manager.officeNumber}</a>
-       </div>
-     </div> 
+
+    <div class="card-content">
+    <div class="media">
+      <div class="media-left">
+      <div class="media-content">
+        <p class="title is-4">Manager:</p>
+        <p class="subtitle is-6">${manager.getName()}</p>
+      </div>
+    </div>
+    <div class="content">
+      <h6>Employee ID:${manager.getId()}</h6>
+      <h6>GitHub:${manager.officeNumber} </h6>.
+      <a href="#"></a> <a href="#">Email: ${manager.getEmail()} </a>
+      <br>
+    </div>
+  </div>
+</div>
+
     `
     }
      const createEngineer = (engineer) => {
      return `
-     <div class="card-body" style="width: 18rem;">
-    <h5 class="card-title">Engineer: ${engineer.name}</h5>
-    <h6 class="card-subtitle mb-2 "></h6>
-    <p class="card-text">Employee ID ${engineer.getId()}</p>
-    <a href="#" class="card-link">Email: ${engineer.getEmail()}</a>
-    <a href="#" class="card-link">GitHub: ${engineer.gitHub}</a>
-  </div>
-</div>
+
+     <div class="card-content">
+     <div class="media">
+       <div class="media-left">
+       <div class="media-content">
+         <p class="title is-4">Engineer:</p>
+         <p class="subtitle is-6">${engineer.name}</p>
+       </div>
+     </div>
+     <div class="content">
+       <h6>Employee ID:${engineer.getId()}</h6>
+       <h6>GitHub:${engineer.gitHub} </h6>.
+       <a href="#"></a> <a href="#">Email: ${engineer.getEmail()} </a>
+       <br>
+     </div>
+   </div>
+ </div>
+
      `
     } 
 
     const createIntern = (intern) => {
     return `  
-<div class="card-body" style="width: 18rem;">
-<h5 class="card-title">Intern: ${intern.getName()}</h5>
-<h6 class="card-subtitle mb-2" ></h6>
-<p class="card-text">Employee ID ${intern.getId()}</p>
-<p class="card-text">School:${intern.school}</p>
-<a href="#" class="card-link">Email: ${intern.getEmail()}</a>
-<a href="#" class="card-link"></a>
-</div>
-</div>
+    
+    <div class="card-content">
+      <div class="media">
+        <div class="media-left">
+        <div class="media-content">
+          <p class="title is-4">Intern</p>
+          <p class="subtitle is-6">${intern.getName()}</p>
+        </div>
+      </div>
+      <div class="content">
+        <h6>Employee ID:${intern.getId()}</h6>
+        <h6>School:${intern.school} </h6>.
+        <a href="#"></a> <a href="#">Email: ${intern.getEmail()} </a>
+        <br>
+      </div>
+    </div>
+  </div>
+
     `
     }
 
@@ -60,7 +89,8 @@ return `
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css">
+       <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma-rtl.min.css">
   <title>Document</title>
 </head>
 <header>
